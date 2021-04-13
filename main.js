4d70a8291e11465ca18f109da54b18e568f9465e
david <david15003@hotmail.com>
2021-04-13 15:42:18 -0500

    Test

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ async function quickstart() {
   /**
    * LLamo el archivo que deseo obtener su texto
    */
-  const fileName = './cedula.jpg'
+  const fileName = './cedula-calidad-baja.jpeg'
 
   // Performs text detection on the local file
   const [result] = await client.textDetection(fileName)
@@ -22,7 +22,7 @@ async function quickstart() {
 
   //Guarda en un json , cambiar nombre
   let data = JSON.stringify(detections)
-  fs.writeFileSync('cedula.json', data)
+  fs.writeFileSync('cedula-calidad-baja-jpeg.json', data)
 }
 
 quickstart()
